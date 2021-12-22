99e47b9f67d82d2a1ea06d031599d16f30bb8131
Kat <kyevsukov@gmail.com>
2021-12-22 10:17:21 -0500

    edit class func

--- api/classes/classes-model.js
+++ api/classes/classes-model.js
@@ -11,11 +11,6 @@ async function getById(class_id) {
 }
 
 async function getUserClasses(user_id) {
-  // select u.user_id, u.username, c.*
-  // from users as u
-  // left join classes_students as cs on cs.student_id = u.user_id
-  // join classes as c on c.class_id = cs.class_id
-  // where u.user_id = 4
   const classes = await db("users as u")
     .select("u.user_id", "u.username", "c.*")
     .leftJoin("classes_students as cs", "cs.student_id", "u.user_id")
@@ -26,38 +21,53 @@ async function getUserClasses(user_id) {
 }
 
 async function getInstructorClasses(user_id) {
-  // select u.user_id, u.username, c.*, count(cs.student_id) as number_registered
-  // from users as u
-  // left join classes as c on u.user_id = c.class_instructor
-  // join classes_students as cs on cs.class_id = c.class_id
-  // where u.user_id = 1
-  // group by u.user_id, u.username, c.class_id
   const classes = await db("users as u")
     .select("u.username", "c.*")
     .count("cs.student_id", { as: "number_registered" })
     .leftJoin("classes as c", "u.user_id", "c.class_instructor")
     .join("classes_students as cs", "cs.class_id", "c.class_id")
     .where("user_id", user_id)
-    .groupBy("u.username", "c.class_id")
-    
-    classes.forEach(cl => cl.number_registered = parseInt(cl.number_registered))
-    return classes
+    .groupBy("u.username", "c.class_id");
+
+  classes.forEach(
+    (cl) => (cl.number_registered = parseInt(cl.number_registered))
+  );
+  return classes;
 }
 
-async function addClass(newClass){
-  const [created] = await db('classes').insert(newClass).returning('*')
-  return created
+async function addClass(newClass) {
+  const [created] = await db("classes").insert(newClass).returning("*");
+  return created;
 }
 
-async function removeClass(class_id){
-  const rowsDeleted = await db('classes').where('class_id', class_id).del()
-  return rowsDeleted
+async function removeClass(class_id) {
+  const rowsDeleted = await db("classes").where("class_id", class_id).del();
+  return rowsDeleted;
 }
 
-async function signupForClass({class_id, student_id}){
-  const [id] = await db('classes_students').insert({class_id, student_id}).returning('student_id')
-  const userClasses = await getUserClasses(id)
-  return userClasses
+async function signupForClass({ class_id, student_id }) {
+  const [id] = await db("classes_students")
+    .insert({ class_id, student_id })
+    .returning("student_id");
+  const userClasses = await getUserClasses(id);
+  return userClasses;
 }
 
-module.exports = { getAll, getById, getUserClasses, getInstructorClasses, addClass, removeClass, signupForClass };
+async function editClass(id, changes) {
+  const [class_id] = await db("classes")
+    .where("class_id", id)
+    .update(changes, ["class_id"]);
+  const updatedClass = await getById(class_id);
+  return updatedClass;
+}
+
+module.exports = {
+  getAll,
+  getById,
+  getUserClasses,
+  getInstructorClasses,
+  addClass,
+  removeClass,
+  signupForClass,
+  editClass,
+};
